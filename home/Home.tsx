02b84fefc13a3e082a7666daa7637efa0aa7716f
rthrflwr <109304879+rthrflwr2@users.noreply.github.com>
2022-12-03 19:02:59 -0500

    Add files via upload

---
+++ home/Home.tsx
@@ -0,0 +1,34 @@
+import styles from './Home.module.scss'
+import Button from '../../components/button/Button'
+import Image from '../../assets/main_background.jpg'
+import Layout from '../layout/Layout'
+import Promo from '../../assets/promo_png.png'
+
+import type { RootState } from '../../store/index'
+import { useSelector, useDispatch } from 'react-redux'
+
+type Props = { buttons: string[] }
+
+function Home() {
+  const stuff = useSelector((state: RootState) => state.movie.buttons)
+
+  console.log(Image)
+
+  return (
+    <>
+      <div
+        className={styles.main}
+        style={{ background: `url("${Image}")`, backgroundRepeat: 'no-repeat' }}
+      >
+        <Layout>
+          <div className={styles.promo}>
+            <img src={Promo} alt="" />
+            {/* <img className={styles.promo_bold} src={Promo} alt="" /> */}
+          </div>
+        </Layout>
+      </div>
+    </>
+  )
+}
+
+export default Home
